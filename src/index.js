beb83d884c0576fbd1fc5e6e39258dc6c62ab1ae
Louie Jake Bell <louiejakebell@gmail.com>
2019-02-04 22:42:05 +0000

    fix error + opacity on hover

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { createStore } from 'redux';
 import styled from 'styled-components';
 
 import App from './components/App';
-import reducer from './reducers/index';
-
-const store = createStore(reducer);
 
 const Wrapper = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
@@ -17,9 +14,7 @@ const Wrapper = styled.div`
 
 render(
   <Wrapper>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </Wrapper>,
   document.getElementById('app')
 );
