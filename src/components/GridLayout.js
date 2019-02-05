11276cb02e12046789d1e9dfc8d6b2ff861fcb44
Louie Jake Bell <louiejakebell@gmail.com>
2019-02-05 18:24:36 +0000

    w/ grid layout

---
+++ src/components/GridLayout.js
@@ -0,0 +1,18 @@
+
+import styled from 'styled-components';
+
+const GridLayout = styled.div`
+  display: flex;
+  -webkit-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -webkit-align-content: center;
+  align-content: center;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 20px;
+  width: 600px;
+  height: 900px;
+`;
+
+export default GridLayout;
